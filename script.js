11db2dfe8fe3ca6233bc16dbe42d6ac6f0dd73cf
Claudio Valdes <claudio.valdes.k@gmail.com>
2024-05-13 01:14:25 -0300

    Modificacion de formulario (a terminar)

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scritp para informar que fue subido
+alert('Estoy funcionando');
+
+let userFile =  document.querySelector('.upload-arch');
+
+userFile.addEventListener('change', ()=>{
+    document.querySelector('#upload-img').innerText = userFile.files[0].name;
+});
+
